76c340f94f6cc90dfbe1bd1aa0ba5fd3af7df5be
Son SW <sons69@naver.com>
2023-04-06 16:30:07 +0900

    refactor : WebSecurityConfigurerAdapter에서 SecurityFilterChain Bean으로 변경

--- src/main/java/clonecoding/photogram/config/SecurityConfig.java
+++ src/main/java/clonecoding/photogram/config/SecurityConfig.java
@@ -1,18 +1,27 @@
 package clonecoding.photogram.config;
 
+import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
+import org.springframework.boot.autoconfigure.security.ConditionalOnDefaultWebSecurity;
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.SecurityFilterChain;
 
 
 @EnableWebSecurity // 해당 파일로 시큐리티를 활성화
-@Configuration // IoC에 등록
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
+@Configuration(proxyBeanMethods = false) // IoC에 등록
+@ConditionalOnDefaultWebSecurity
+@ConditionalOnWebApplication(type = ConditionalOnWebApplication.Type.SERVLET)
+public class SecurityConfig {
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        // super 삭제 => 기존 시큐리티가 가지고 있는 기능이 비활성화
+    @Bean
+    @Order(SecurityProperties.BASIC_AUTH_ORDER)
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+        http.csrf().disable();
         http.authorizeHttpRequests()
                 .antMatchers("/","/user/**", "/image/**", "/subscribe/**", "/comment/**").authenticated()
                 .anyRequest().permitAll()
@@ -20,5 +29,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .formLogin()
                 .loginPage("/auth/signin")
                 .defaultSuccessUrl("/");
+
+        return http.build();
     }
+
+
 }
