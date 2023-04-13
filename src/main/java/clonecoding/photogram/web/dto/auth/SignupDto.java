a25f6c1eef21ca41c303ae1cdb278443c98c43ba
submemb <sons6488@gmail.com>
2023-04-13 15:01:22 +0900

    fix : 데이터를 객체에 매핑해주는 toEntity() 추가

--- src/main/java/clonecoding/photogram/web/dto/auth/SignupDto.java
+++ src/main/java/clonecoding/photogram/web/dto/auth/SignupDto.java
@@ -2,6 +2,7 @@ package clonecoding.photogram.web.dto.auth;
 
 //Dto는 통신에 필요한 데이터를 담는거
 
+import clonecoding.photogram.domain.user.User;
 import lombok.Data;
 
 @Data
@@ -10,4 +11,13 @@ public class SignupDto {
     private String password;
     private String email;
     private String name;
+
+    public User toEntity(){
+        return User.builder()
+                .username(username)
+                .password(password)
+                .email(email)
+                .name(name)
+                .build();
+    }
 }
