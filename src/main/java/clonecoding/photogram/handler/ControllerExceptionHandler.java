14b51b83372d44833c9d54a5692b20c542332fb7
Son SW <sons69@naver.com>
2023-04-30 21:01:40 +0900

    add : 공통응답 script 형식

--- src/main/java/clonecoding/photogram/handler/ControllerExceptionHandler.java
+++ src/main/java/clonecoding/photogram/handler/ControllerExceptionHandler.java
@@ -1,6 +1,7 @@
 package clonecoding.photogram.handler;
 
 import clonecoding.photogram.handler.ex.CustomValidationException;
+import clonecoding.photogram.util.Script;
 import clonecoding.photogram.web.dto.CMRespDto;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -12,8 +13,17 @@ import java.util.Map;
 @ControllerAdvice
 public class ControllerExceptionHandler {
 
+//    @ExceptionHandler(CustomValidationException.class)
+//    public CMRespDto<?> validationException(CustomValidationException e){
+//        return new CMRespDto<Map<String, String>>(-1, e.getMessage(), e.getErrorMap());
+//    }
+
     @ExceptionHandler(CustomValidationException.class)
-    public CMRespDto<?> validationException(CustomValidationException e){
-        return new CMRespDto<Map<String, String>>(-1, e.getMessage(), e.getErrorMap());
+    public String validationException(CustomValidationException e){
+        // CMRespDto 비교
+        // 1. 클라이언트에게 응답할때는 Script가 좋음
+        // 2. Ajax 통신 - CMRespDto
+        // 3. Android통신 - CMRespDto
+        return Script.back(e.getErrorMap().toString());
     }
 }
