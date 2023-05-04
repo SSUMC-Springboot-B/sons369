be41e8c89771beb1339218f3c7d124dd086ddcdf
Son SW <sons69@naver.com>
2023-05-05 00:15:09 +0900

    fix : 유저네임으로 회원 조회

--- src/main/java/clonecoding/photogram/domain/user/UserRepository.java
+++ src/main/java/clonecoding/photogram/domain/user/UserRepository.java
@@ -4,4 +4,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 //어노테이션 없어도 JpaRepository 상속받으면 자동으로 등록해줌
 public interface UserRepository extends JpaRepository<User, Integer> {
+
+    //JPA 쿼리 메서드
+    User findByUsername(String username);
+
 }
