8da72dae23b42daae30b910a87a15963d4b5b82a
Son SW <sons69@naver.com>
2023-05-14 00:44:35 +0900

    add : 구독서비스 추가

---
+++ src/main/java/clonecoding/photogram/service/SubscribeService.java
@@ -0,0 +1,24 @@
+package clonecoding.photogram.service;
+
+import clonecoding.photogram.domain.subscribe.Subscribe;
+import clonecoding.photogram.domain.subscribe.SubscribeRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class SubscribeService {
+
+    private final SubscribeRepository subscribeRepository;
+
+    @Transactional
+    public void 구독하기(int fromUserId, int toUserId){
+        subscribeRepository.mSubscribe(fromUserId, toUserId);
+    }
+
+    @Transactional
+    public void 구독취소하기(int fromUserId, int toUserId){
+        subscribeRepository.mUnSubscribe(fromUserId, toUserId);
+    }
+}
