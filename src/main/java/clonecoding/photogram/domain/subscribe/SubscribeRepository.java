181f47a8932db7cf49d38637e3da9d53df4b935a
Son SW <sons69@naver.com>
2023-05-14 00:44:21 +0900

    fix : 네이티브 쿼리 메서드 추가

--- src/main/java/clonecoding/photogram/domain/subscribe/SubscribeRepository.java
+++ src/main/java/clonecoding/photogram/domain/subscribe/SubscribeRepository.java
@@ -1,6 +1,8 @@
 package clonecoding.photogram.domain.subscribe;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
 import javax.persistence.Column;
 import javax.persistence.Table;
@@ -8,4 +10,11 @@ import javax.persistence.UniqueConstraint;
 
 public interface SubscribeRepository extends JpaRepository<Subscribe, Integer> {
 
+    @Modifying //INSERT, DELETE, UPDATE를 네이티브 쿼리로 작성할려면 해당 애노테이션 필요
+    @Query(value="INSERT INTO subscribe(fromUserId, toUserId, createDate) VALUES(:fromUserId, :toUserId, now())", nativeQuery = true)
+    void mSubscribe(int fromUserId, int toUserId); //성공시 +1, 실패시 -1 (변경의 횟수만큼 +1됨)
+
+    @Modifying
+    @Query(value="DELETE FROM subscribe WHERE fromUserId= :fromUserId AND toUserId = :toUserId", nativeQuery = true)
+    void mUnSubscribe(int fromUserId, int toUserId);
 }
